c0c4d6c4aa7e6c594005ddab40927b97463065a1
KenTr9989 <kentr9989@gmail.com>
2024-06-07 18:15:48 +1000

    implement Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './navbar.module.css';
 import { Link } from 'react-router-dom';
-
+import { AiOutlineUser, AiOutlineShoppingCart } from 'react-icons/ai';
 const Navbar = () => {
   return (
     <div className={classes.container}>
@@ -19,9 +19,25 @@ const Navbar = () => {
             <li className={classes.listItem}>
               <a href='#contacts'>Contacts</a>
             </li>
+            <li className={classes.listItem}>
+              <a href='#foods'>Foods</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href='#faq'>FAQ</a>
+            </li>
+            <li className={classes.listItem}>
+              <Link to='/create'>Create</Link>
+            </li>
           </ul>
         </div>
-        <div className={classes.right}></div>
+        <div className={classes.right}>
+          <AiOutlineUser className={classes.userIcon} />
+          <Link to='/cart' className={classes.cartContainer}>
+            <AiOutlineShoppingCart className={classes.cartIcon} />
+            <div className={classes.cartQuantity}>0</div>
+          </Link>
+          <button className={classes.cartLogout}>Logout</button>
+        </div>
       </div>
     </div>
   );
