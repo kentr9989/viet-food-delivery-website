1cdcb35394e2fdcf963563da76dbf924b3eb306d
KenTr9989 <kentr9989@gmail.com>
2024-06-08 23:04:07 +1000

    implement signin responsive

--- client/src/components/signin/SignIn.jsx
+++ client/src/components/signin/SignIn.jsx
@@ -1,7 +1,72 @@
 import React from 'react';
 import classes from './signin.module.css';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import img from '../../assets/womaneating.jpg';
+import { login } from '../../redux/authSlice';
+
 const SignIn = () => {
-  return <div></div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`http://localhost:4500/auth/login`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await res.json();
+      console.log(data);
+      dispatch(login(data));
+      navigate('/');
+    } catch (error) {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 5000);
+    }
+  };
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <img src={img} className={classes.leftImage} />
+        </div>
+        <div className={classes.right}>
+          <h2 className={classes.title}>Login</h2>
+          <form onSubmit={handleLogin} className={classes.loginForm}>
+            <input
+              type='email'
+              placeholder='Email'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type='password'
+              placeholder='Pasword'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className={classes.submitBtn}>Login</button>
+            <p>
+              Don't have account ? <Link to='/signup'>Sign up</Link>
+            </p>
+            {error && (
+              <div className={classes.errorMsg}> Wrong email or password.</div>
+            )}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
